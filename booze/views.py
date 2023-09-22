f3d62de6d73ed11d125d27cbd1a958e0f9ac68eb
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-22 06:12:29 -0400

    post data

--- booze/views.py
+++ booze/views.py
@@ -1,14 +1,24 @@
 from django.http import JsonResponse
 from .models import Booze
 from .serializers import BoozeSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
-def booze_list(response):
-
-  # get all booze
-  booze = Booze.objects.all()
-  # serialize the booze
-  serializer = BoozeSerializer(booze, many=True)
-  # return in JSON format
-  # return JsonResponse(serializer.data, safe=False)
-  # return as object -- can remove the safe=False param6
-  return JsonResponse({'booze' : serializer.data})
+@api_view(http_method_names=['GET', 'POST'])
+def booze_list(request):
+  if request.method == "GET":
+    # get all booze
+    booze = Booze.objects.all()
+    # serialize the booze
+    serializer = BoozeSerializer(booze, many=True)
+    # return in JSON format
+    # return JsonResponse(serializer.data, safe=False)
+    # return as object -- can remove the safe=False param6
+    return JsonResponse({'booze' : serializer.data})
+  
+  if request.method == "POST":
+    serializer = BoozeSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
